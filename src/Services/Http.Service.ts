33e8442c781106437ae1078252beebe27a2379a1
Exitare <business.exitare@icloud.com>
2020-04-25 12:09:04 -0700

    Updated url

--- src/Services/Http.Service.ts
+++ src/Services/Http.Service.ts
@@ -6,7 +6,7 @@ export class HttpService {
 
     public static async get(): Promise<any> {
         return new Promise(async (resolve) => {
-            axios.get("https://corona.lmao.ninja/countries")
+            axios.get("https://corona.lmao.ninja/v2/countries")
                 .then((response: any) => {
                     resolve(response.data);
                 }).catch((err: any) => console.log(err));
